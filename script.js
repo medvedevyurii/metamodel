915d74c449a0f0ae91143d21dc1a2ea0c578c6c4
kosolapii <kosolapii84@ya.ru>
2022-05-31 13:17:41 +0300

    first mcommit 31_05

---
+++ script.js
@@ -0,0 +1,16 @@
+// Подгонка ывсоты фрейма под содержимое
+var frame = document.getElementById("Iframe");
+frame.onload = function(){
+    frame.style.height = frame.contentWindow.document.body.scrollHeight + 'px';
+    // frame.style.width = frame.contentWindow.document.body.scrollWidth+'px';
+}
+
+// Скрытие бургер-меню после нажатия
+let menuItems=document.querySelectorAll('.nav-link');
+for (menuItem of menuItems){
+        menuItem.onclick=()=>{
+            document.getElementById('button-menu').classList.add('collapsed');
+            document.getElementById('button-menu').setAttribute("aria-expanded", "false");
+            document.getElementById('navbarSupportedContent').classList.remove('show');
+    };
+};
